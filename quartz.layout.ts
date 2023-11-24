69630fa1edd449eb09811a5a485be0de6e73eb03
RookOz <royozx@gmail.com>
2023-11-24 10:03:07 +0200

    Quartz sync: Nov 24, 2023, 10:03 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    //Component.Graph(),
+	Component.Explorer(),
     Component.DesktopOnly(Component.TableOfContents()),
 	//Component.TableOfContents(),
     Component.Backlinks(),
